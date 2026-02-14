9380cfb8bf28af4106308128dcab2057c1488826
Drelka <47503020+Drelka@users.noreply.github.com>
2026-02-14 14:17:04 +0000

    add END button with session history

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const startButton = document.querySelector('.start-button');
 const counterDisplay = document.querySelector('#counter');
 const buttonDiv = document.querySelector('.buttons');
-
-const resetButton = document.createElement('button');
-resetButton.classList.add('button');
-resetButton.textContent = "Reset";
-buttonDiv.appendChild(resetButton);
+const resetButton = document.querySelector('.reset-button');
+const endButton = document.querySelector('.end-button');
+const storage = document.querySelector('#storage');
 
 startButton.addEventListener('click', startCounting); // start/pause/resume
 resetButton.addEventListener('click', resetCounter);
+endButton.addEventListener('click', endSession);
 
 let seconds = 0;
 let intervalID = null;
@@ -20,11 +19,11 @@ function startCounting() {
             seconds += 1;
             counterDisplay.textContent = seconds;
         }, 1000);
-        startButton.textContent = 'Pause';
+        startButton.textContent = 'PAUSE';
     } else if (typeof(intervalID) === 'number') {
         clearInterval(intervalID);
         intervalID = null;
-        startButton.textContent = 'Resume';
+        startButton.textContent = 'RESUME';
     }
 }
 
@@ -32,9 +31,20 @@ function resetCounter() {
     console.log('reset button clicked');
     seconds = 0;
     counterDisplay.textContent = seconds;
+    startButton.textContent = 'START';
     if (intervalID !== null) {
         clearInterval(intervalID);
         intervalID = null;
-        startButton.textContent = 'Start';
+    }
+}
+
+function endSession() {
+    console.log('endSession button clicked');
+
+    if (seconds !== 0) {
+        let session = document.createElement('li');
+        storage.appendChild(session);
+        session.textContent = seconds;
+        resetCounter();
     }
 }
